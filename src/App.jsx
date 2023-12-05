69ec86d5176f7ae58f74773a3f172a2257d68be8
JimOluwaseyi <heysey128@gmail.com>
2023-12-05 11:49:12 +0100

    Add the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import Calculator from "./components/Calculator";
-import Quotes from "./components/Quotes";
+// eslint-disable-next-line
+
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Calculator from './components/Calculator';
+import Header from './components/Header';
+import Quotes from './components/Quotes';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="container">
-      <Calculator />
-      <Quotes />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quotes />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Math Magicians</h1>
+      <nav>
+        <Link className="links" to="/">Home</Link>
+        <Link className="links" to="/calculator">Calculator</Link>
+        <Link className="links" to="/quote">Quote</Link>
+      </nav>
+    </div>
+  );
+}
